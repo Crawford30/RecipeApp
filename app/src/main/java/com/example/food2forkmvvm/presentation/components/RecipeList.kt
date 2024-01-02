e9d24a42ea76009604328046e9d56fadfaaea388
Joel Crawford <joelcrawford30@gmail.com>
2024-01-02 18:09:08 +0300

    Detail Data

--- app/src/main/java/com/example/food2forkmvvm/presentation/components/RecipeList.kt
+++ app/src/main/java/com/example/food2forkmvvm/presentation/components/RecipeList.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.ExperimentalMaterialApi
@@ -36,6 +37,7 @@ fun RecipeList(
     onNavigateToRecipeDetailScreen: (String) -> Unit,
 ){
     Box(modifier = Modifier
+        .padding(8.dp)
         .background(color = MaterialTheme.colors.surface)
     ) {
         if (loading && recipes.isEmpty()) {
